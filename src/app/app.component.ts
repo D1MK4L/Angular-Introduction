a7b01984b470b0878aef4b0da07c07bb87e41554
D1MK4L <Kaloudis_dimitrios@yahoo.com>
2024-03-29 18:12:09 +0200

    New object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,14 @@
 import { Component } from '@angular/core';
+import { PersonTableComponent } from './components/person-table/person-table.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [PersonTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   name = 'D1MK4L';
-
-  person = {
-    givenName: 'D1MK4L',
-    surName: 'Gustaffer',
-    age: 0x2c,
-    email: 'Kaloudis_dimitrios@yahoo.com',
-  };
 }
