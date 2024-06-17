319c9246c738ce10571428a3a0ff894dc03098dd
D1MK4L <Kaloudis_dimitrios@yahoo.com>
2024-06-17 19:24:25 +0300

    !

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -12,6 +12,11 @@ import { UserRegistrationComponent } from './components/user-registration/user-r
 import { RestrictedContentExampleComponent } from './components/restricted-content-example/restricted-content-example.component';
 import { authGuard } from './shared/guards/auth.guard';
 import { UserLoginComponent } from './components/user-login/user-login.component';
+import { CrudDashboardComponent } from './components/crud/crud-dashboard/crud-dashboard.component';
+import { CrudCreateExampleComponent } from './components/crud/crud-create-example/crud-create-example.component';
+import { CrudReadExampleComponent } from './components/crud/crud-read-example/crud-read-example.component';
+import { CrudUpdateExampleComponent } from './components/crud/crud-update-example/crud-update-example.component';
+import { CrudDeleteExampleComponent } from './components/crud/crud-delete-example/crud-delete-example.component';
 
 export const routes: Routes = [    
     {
@@ -36,5 +41,20 @@ export const routes: Routes = [
       canActivate: [authGuard],
       },
       {path: 'login', component: UserLoginComponent},
+      {path: 'crud-example', component: CrudDashboardComponent,
+        canActivate: [authGuard],
+      },
+      {path: 'crud-example/create', component: CrudCreateExampleComponent,
+      canActivate: [authGuard],
+       },
+      {path: 'crud-example/read', component: CrudReadExampleComponent,
+      canActivate: [authGuard],
+      },
+      {path: 'crud-example/update', component: CrudUpdateExampleComponent,
+      canActivate: [authGuard],
+      },
+      {path: 'crud-example/delete', component: CrudDeleteExampleComponent,
+      canActivate: [authGuard],
+      },
       { path: '', component: WelcomeComponent },      
     ];
